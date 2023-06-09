4cc1be67ef10b3c20f119eb998ad10c9a4ed2921
Laura <mobttcode@gmail.com>
2023-06-09 16:04:54 +0200

    creation des boutons de filtre

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -6,17 +6,41 @@ async function getWorks() {
 }
 
 
-// AJOUTS A LA GALLERIE
+// AJOUTS TRAVAUX A LA GALLERIE
 const gallery = document.querySelector(".gallery");
 
 async function generateGallery() {
-    await getWorks()
+    await getWorks();
 
     works.forEach(works => {
-        let work = document.createElement("figure")
+        let work = document.createElement("figure");
         work.innerHTML += `<img src="${works.imageUrl}" alt="${works.title}"><figcaption>${works.title}</figcaption>`;
         gallery.appendChild(work);
     })
 }
 generateGallery()
 
+
+// ---FILTRES---
+const filters = document.querySelector(".filters");
+
+async function generateFiltersBtns() {
+    await getWorks();
+
+    // RECUPERATION DES NOMS DE CATEGORIES
+    let categoryNameSet = new Set();
+    for (let i = 0; i < works.length; i++) {
+        categoryNameSet.add("Tous")
+        categoryNameSet.add(works[i].category.name)
+    }
+    let categoryName = [...categoryNameSet];
+
+    // CREATION DES BOUTONS
+    for (let i = 0; i < categoryNameSet.size; i++) {
+    let filterBtn = document.createElement("button");
+    filters.appendChild(filterBtn);
+    filterBtn.classList.add("filterbtn");
+    filterBtn.textContent = categoryName[i];
+    }
+}
+generateFiltersBtns()
